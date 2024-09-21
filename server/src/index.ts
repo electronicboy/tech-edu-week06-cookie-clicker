ed1f848e26e8cf3115b259a8ef1cea13e13fc370
Shane Freeder <theboyetronic@gmail.com>
2024-09-21 23:15:32 +0100

    More data complexity, not worth supporting adding right now

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,32 +21,6 @@ app.get("/api/upgrades", (req: Request, res: Response) => {
         })
 })
 
-app.post("/api/upgrades", (req: Request, res: Response) => {
-    const {name, cost, increase} = req.body;
-    if (!name || !cost || !increase) {
-        res.status(400).json({"message": "POST body requires a name, cost and an increase value"})
-        return;
-    }
-    if (cost < 0) {
-        res.status(400).json({"message": "cost must be a number above zero"})
-        return;
-    }
-
-    if (increase < 0) {
-        res.status(400).json({"message": "increase must be a number above zero"})
-        return;
-    }
-
-    dataSource.addUpgrade(name, cost, increase)
-        .then(upgrade => {
-            res.json(upgrade)
-        })
-        .catch((err: Error) => {
-            res.status(500).json({"message": err.message}).end();
-        })
-
-})
-
 const port = process.env.PORT || 8081;
 app.listen(port, () => {
     console.log("Server running on port: " + port);
